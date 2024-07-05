e23cdcc6660a61acb993e985e2b72978416663e8
Candy Ho <candyho.learning@gmail.com>
2024-07-05 23:04:35 +0800

    added flip animation and tweaked styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 import "./App.css";
 import HeroSection from "./components/HeroSection";
 import SubNav from "./components/SubNav";
--- src/components/Circular.jsx
+++ src/components/Circular.jsx
@@ -1,15 +1,16 @@
 import circularFront from "/circular-front.png";
 import reactLogo from "../assets/react.svg";
+import "../styles/circular.css";
 
 export default function Circular() {
   return (
     <section className="circular">
       <div className="flip-menu centered">
         <div className="card">
-          <div className="card-front">
+          <div className="card-front centered">
             <img src={circularFront} alt="" className="circular-front" />
           </div>
-          <div className="card-back">
+          <div className="card-back centered">
             <img src={reactLogo} alt="" className="circular-back" />
           </div>
         </div>
