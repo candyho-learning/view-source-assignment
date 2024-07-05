3bbb67b4c061237994d0d73d9e9b71cd5c81cd2e
Candy Ho <candyho.learning@gmail.com>
2024-07-05 23:34:50 +0800

    style: added different brand name counts and styles on desktop

--- src/components/OurBrands.jsx
+++ src/components/OurBrands.jsx
@@ -1,14 +1,27 @@
 import BrandGroup from "./BrandGroup";
 const brands = Array.from({ length: 5 }, (v, _) => "Lorem Ipsum");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const brandCounts = [
+  5, 7, 9, 7, 5, 5, 3, 7, 5, 3, 7, 5, 9, 3, 7, 5, 3, 7, 5, 9, 3, 5, 7, 3, 7, 5,
+];
+
+console.log(brandCounts.length);
 
 export default function OurBrands() {
   return (
     <section className="our-brands">
       <h3 className="our-brands-title">Our Brands</h3>
       <div className="brand-group-wrapper">
-        <BrandGroup title="A" brandsArr={brands} />
-        <BrandGroup title="B" brandsArr={brands} />
-        <BrandGroup title="C" brandsArr={brands} />
+        {alphabet.split("").map((letter, i) => (
+          <BrandGroup
+            key={letter}
+            title={letter}
+            brandsArr={Array.from(
+              { length: brandCounts[i] },
+              (v, _) => "Lorem Ipsum"
+            )}
+          />
+        ))}
       </div>
     </section>
   );
